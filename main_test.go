a727a2bee3cd25b54ac7f4a47acb30032c4ad92e
zero-coolio <steve@nullEffect.com>
2024-01-11 07:56:44 -0500

    testing

--- main_test.go
+++ main_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestServer(t *testing.T) {
 	fmt.Print("testing server")
+	assert.Equal(t, 123, 123, "they should be equal")
 }
